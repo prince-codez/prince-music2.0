43328ee3c95c8b55d653c6d5c9852338656d0520
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2024-10-21 12:11:44 +0530

    Update and rename Mmmm to couples.py

--- TanuMusic/plugins/tools/couples.py
+++ TanuMusic/plugins/tools/couples.py
@@ -8,8 +8,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import *
 
 #BOT FILE NAME
-from DAXXMUSIC import app as app
-from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
+from Tanumusic import app as app
+from Tanumusic.mongo.couples_db import _get_image, get_couple
 
 POLICE = [
     [
