bade909a0401dddff4cf6afc81db477df910c2f4
ꭙ Kʀɪsʜ ˼ <167740982+Speedsnet@users.noreply.github.com>
2024-11-11 23:08:19 +0530

    𝐓ᴇᴀᴍᴋʀɪsʜ

--- config/config.py
+++ config/config.py
@@ -31,7 +31,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600000))
 
 #❖  Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002115990090"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002115990090"))
 
 #❖ Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "7453278496"))
